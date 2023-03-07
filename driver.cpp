69b48b8e207747eb27a7fc913bb4e0cb4473ba45
Matt Norman <normanmr@ornl.gov>
2023-03-07 13:04:47 -0500

    Simplifying the driver and still getting the hang

--- driver.cpp
+++ driver.cpp
@@ -39,20 +39,13 @@ int main(int argc, char** argv) {
 
     mainproc = (myrank == 0);
 
-    bool sim2d = ny_glob == 1;
-
-    if (sim2d) {
-      nproc_x = nranks;
-      nproc_y = 1;
-    } else {
-      // Find integer nproc_y * nproc_x == nranks such that nproc_y and nproc_x are as close as possible
-      nproc_y = (int) std::ceil( std::sqrt((double) nranks) );
-      while (nproc_y >= 1) {
-        if (nranks % nproc_y == 0) { break; }
-        nproc_y--;
-      }
-      nproc_x = nranks / nproc_y;
+    // Find integer nproc_y * nproc_x == nranks such that nproc_y and nproc_x are as close as possible
+    nproc_y = (int) std::ceil( std::sqrt((double) nranks) );
+    while (nproc_y >= 1) {
+      if (nranks % nproc_y == 0) { break; }
+      nproc_y--;
     }
+    nproc_x = nranks / nproc_y;
 
     // Get my ID within each dimension's number of ranks
     py = myrank / nproc_x;
@@ -84,23 +77,8 @@ int main(int argc, char** argv) {
 
     real4d state  ("state"  ,num_state  ,nz+2*hs,ny+2*hs,nx+2*hs);
     real4d tracers("tracers",num_tracers,nz+2*hs,ny+2*hs,nx+2*hs);
-    real1d hy_dens_cells      ("hy_dens_cells"      ,nz  );
-    real1d hy_dens_theta_cells("hy_dens_theta_cells",nz  );
-    real1d hy_dens_edges      ("hy_dens_edges"      ,nz+1);
-    real1d hy_dens_theta_edges("hy_dens_theta_edges",nz+1);
-    real4d state_tmp   ("state_tmp"   ,num_state  ,nz+2*hs,ny+2*hs,nx+2*hs);
-    real4d state_tend  ("state_tend"  ,num_state  ,nz     ,ny     ,nx     );
-    real4d tracers_tmp ("tracers_tmp" ,num_tracers,nz+2*hs,ny+2*hs,nx+2*hs);
-    real4d tracers_tend("tracers_tend",num_tracers,nz     ,ny     ,nx     );
-
-    realHost4d halo_send_buf_W_host("halo_send_buf_W_host",npack,nz,ny,hs);
-    realHost4d halo_send_buf_E_host("halo_send_buf_E_host",npack,nz,ny,hs);
-    realHost4d halo_send_buf_S_host("halo_send_buf_S_host",npack,nz,hs,nx);
-    realHost4d halo_send_buf_N_host("halo_send_buf_N_host",npack,nz,hs,nx);
-    realHost4d halo_recv_buf_S_host("halo_recv_buf_S_host",npack,nz,hs,nx);
-    realHost4d halo_recv_buf_N_host("halo_recv_buf_N_host",npack,nz,hs,nx);
-    realHost4d halo_recv_buf_W_host("halo_recv_buf_W_host",npack,nz,ny,hs);
-    realHost4d halo_recv_buf_E_host("halo_recv_buf_E_host",npack,nz,ny,hs);
+    state = 1;
+    tracers = 1;
 
     real4d halo_send_buf_W("halo_send_buf_W",npack,nz,ny,hs);
     real4d halo_send_buf_E("halo_send_buf_E",npack,nz,ny,hs);
@@ -118,20 +96,15 @@ int main(int argc, char** argv) {
     real4d halo_send_buf_S("halo_send_buf_S",npack,nz,hs,nx);
     real4d halo_send_buf_N("halo_send_buf_N",npack,nz,hs,nx);
 
-    if (!sim2d) {
-      parallel_for( YAKL_AUTO_LABEL() , Bounds<4>(npack,nz,hs,nx) , YAKL_LAMBDA (int v, int k, int jj, int i) {
-        if (v < num_state) {
-          halo_send_buf_S(v,k,jj,i) = state  (v          ,hs+k,hs+jj,hs+i);
-          halo_send_buf_N(v,k,jj,i) = state  (v          ,hs+k,ny+jj,hs+i);
-        } else {
-          halo_send_buf_S(v,k,jj,i) = tracers(v-num_state,hs+k,hs+jj,hs+i);
-          halo_send_buf_N(v,k,jj,i) = tracers(v-num_state,hs+k,ny+jj,hs+i);
-        }
-      });
-    }
-
-    yakl::fence();
-    yakl::timer_start("halo_exchange_mpi");
+    parallel_for( YAKL_AUTO_LABEL() , Bounds<4>(npack,nz,hs,nx) , YAKL_LAMBDA (int v, int k, int jj, int i) {
+      if (v < num_state) {
+        halo_send_buf_S(v,k,jj,i) = state  (v          ,hs+k,hs+jj,hs+i);
+        halo_send_buf_N(v,k,jj,i) = state  (v          ,hs+k,ny+jj,hs+i);
+      } else {
+        halo_send_buf_S(v,k,jj,i) = tracers(v-num_state,hs+k,hs+jj,hs+i);
+        halo_send_buf_N(v,k,jj,i) = tracers(v-num_state,hs+k,ny+jj,hs+i);
+      }
+    });
 
     real4d halo_recv_buf_W("halo_recv_buf_W",npack,nz,ny,hs);
     real4d halo_recv_buf_E("halo_recv_buf_E",npack,nz,ny,hs);
@@ -145,35 +118,22 @@ int main(int argc, char** argv) {
 
     yakl::fence();
 
-    //Pre-post the receives
-    MPI_Irecv( halo_recv_buf_W.data() , halo_recv_buf_W.size() , mpi_data_type , neigh(1,0) , 0 , MPI_COMM_WORLD , &rReq[0] );
-    MPI_Irecv( halo_recv_buf_E.data() , halo_recv_buf_E.size() , mpi_data_type , neigh(1,2) , 1 , MPI_COMM_WORLD , &rReq[1] );
-    if (!sim2d) {
-      MPI_Irecv( halo_recv_buf_S.data() , halo_recv_buf_S.size() , mpi_data_type , neigh(0,1) , 2 , MPI_COMM_WORLD , &rReq[2] );
-      MPI_Irecv( halo_recv_buf_N.data() , halo_recv_buf_N.size() , mpi_data_type , neigh(2,1) , 3 , MPI_COMM_WORLD , &rReq[3] );
-    }
+    MPI_Irecv( halo_recv_buf_W.data() , halo_recv_buf_W.size() , MPI_DOUBLE , neigh(1,0) , 0 , MPI_COMM_WORLD , &rReq[0] );
+    MPI_Irecv( halo_recv_buf_E.data() , halo_recv_buf_E.size() , MPI_DOUBLE , neigh(1,2) , 1 , MPI_COMM_WORLD , &rReq[1] );
+    MPI_Irecv( halo_recv_buf_S.data() , halo_recv_buf_S.size() , MPI_DOUBLE , neigh(0,1) , 2 , MPI_COMM_WORLD , &rReq[2] );
+    MPI_Irecv( halo_recv_buf_N.data() , halo_recv_buf_N.size() , MPI_DOUBLE , neigh(2,1) , 3 , MPI_COMM_WORLD , &rReq[3] );
 
-    //Send the data
-    MPI_Isend( halo_send_buf_W.data() , halo_send_buf_W.size() , mpi_data_type , neigh(1,0) , 1 , MPI_COMM_WORLD , &sReq[0] );
-    MPI_Isend( halo_send_buf_E.data() , halo_send_buf_E.size() , mpi_data_type , neigh(1,2) , 0 , MPI_COMM_WORLD , &sReq[1] );
-    if (!sim2d) {
-      MPI_Isend( halo_send_buf_S.data() , halo_send_buf_S.size() , mpi_data_type , neigh(0,1) , 3 , MPI_COMM_WORLD , &sReq[2] );
-      MPI_Isend( halo_send_buf_N.data() , halo_send_buf_N.size() , mpi_data_type , neigh(2,1) , 2 , MPI_COMM_WORLD , &sReq[3] );
-    }
+    MPI_Isend( halo_send_buf_W.data() , halo_send_buf_W.size() , MPI_DOUBLE , neigh(1,0) , 1 , MPI_COMM_WORLD , &sReq[0] );
+    MPI_Isend( halo_send_buf_E.data() , halo_send_buf_E.size() , MPI_DOUBLE , neigh(1,2) , 0 , MPI_COMM_WORLD , &sReq[1] );
+    MPI_Isend( halo_send_buf_S.data() , halo_send_buf_S.size() , MPI_DOUBLE , neigh(0,1) , 3 , MPI_COMM_WORLD , &sReq[2] );
+    MPI_Isend( halo_send_buf_N.data() , halo_send_buf_N.size() , MPI_DOUBLE , neigh(2,1) , 2 , MPI_COMM_WORLD , &sReq[3] );
 
     MPI_Status  sStat[4];
     MPI_Status  rStat[4];
 
-    //Wait for the sends and receives to finish
-    if (sim2d) {
-      MPI_Waitall(2, sReq, sStat);
-      MPI_Waitall(2, rReq, rStat);
-    } else {
-      MPI_Waitall(4, sReq, sStat);
-      MPI_Waitall(4, rReq, rStat);
-    }
+    MPI_Waitall(4, sReq, sStat);
+    MPI_Waitall(4, rReq, rStat);
     yakl::fence();
-    yakl::timer_stop("halo_exchange_mpi");
 
   }
   yakl::finalize();
